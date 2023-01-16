5ea60d52d1470461d165c2ff8f19710186cbdefb
Dennis Nichols <dgnichols22@gmail.com>
2023-01-16 14:11:24 -0600

    requests hotfix

--- assets/get_eth_price_data.py
+++ assets/get_eth_price_data.py
@@ -22,6 +22,7 @@ api_key = os.environ.get('COIN_API_KEY')
 url = 'https://rest.coinapi.io/v1/exchangerate/ETH/USD/history?period_id=1HRS&time_start=2021-01-01T00%3A00%3A00&time_end=2022-01-01T00%3A00%3A00'
 headers = {'X-CoinAPI-Key' : api_key}
 
+response = requests.get(url, headers= headers)
 
 # convert response data to json and then into pandas df
 response_json = response.json()
@@ -40,5 +41,7 @@ eth_history_df['time_open'] = eth_history_df['time_open'].apply(make_unix)
 eth_history_df['rate_open'] = eth_history_df['rate_open'].apply(round_2)
 
 eth_history_df = eth_history_df.rename(columns={'rate_open': 'eth_price', 'time_open': 'timestamp'})
+
+print(eth_history_df)
 
 eth_history_df.to_csv('eth_price_history.csv')
