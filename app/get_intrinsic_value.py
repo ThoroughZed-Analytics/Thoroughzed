2be02a5d3821b15def921ce9203ccba9f8187f91
Harper Foley <harper.e.foley@gmail.com>
2023-01-18 10:59:10 -0800

    Adds in logic to keep run_cli() in a loop until user decides exit. This allows users to conduct multiple intrinsic valuations without the script stopping. Additionally, added in logic to handle unraced horses. Since their races are 0, we cannot project cash flows and returns.

--- app/get_intrinsic_value.py
+++ app/get_intrinsic_value.py
@@ -6,6 +6,8 @@ def get_intrinsic_value(id, horse_cost):
     # extract data from horse
     df = get_summary_horse_data(id)
     total_races = df['data.horse.race_statistic.number_of_races'][0]
+    if total_races == 0:
+        return 'Unraced'
     first_place_percent = df['data.horse.race_statistic.win_rate'][0]
     second_place_percent = df['data.horse.race_statistic.second_place_finishes'][0] / total_races * 100
     third_place_percent = df['data.horse.race_statistic.third_place_finishes'][0] / total_races * 100
@@ -13,6 +15,7 @@ def get_intrinsic_value(id, horse_cost):
     total_paid_races = df['data.horse.race_statistic.number_of_paid_races'][0]
     first_place_percent_free = df['data.horse.race_statistic.free_win_rate'][0]
     first_place_percent_paid = df['data.horse.race_statistic.paid_win_rate'][0]
+    
 
     # Potential net earnings for each race (takes into account entry fee)
     per_race_net_win = ((6 * first_place_percent / 100) + (3 * second_place_percent / 100) + (2 * third_place_percent / 100)) - 1
