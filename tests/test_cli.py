804cc9a01f83f30aa93a53f28801f65c868af810
Oliver Speir <oliverspeir9@gmail.com>
2023-01-18 17:07:02 -0700

    tests q function

---
+++ tests/test_cli.py
@@ -0,0 +1,17 @@
+import pytest
+from app.thoroughzed_cli import run_cli
+from io import StringIO
+
+
+def test_cli_quit(monkeypatch):
+    with pytest.raises(SystemExit) as e:
+        monkeypatch.setattr('builtins.input', lambda _:'q')
+        run_cli()
+    assert e.type == SystemExit
+
+# def test_cli_help(monkeypatch,capfd):
+#     with pytest.raises(SystemExit) as e:
+#         monkeypatch.setattr('builtins.input', lambda _: '1000')
+#         run_cli()
+#         out,err = capfd.readouterr()
+#     assert out == "Would you like to find the (r)elative value or (i)ntrinsic value? Or type 'q' to quit. Or h for help"
