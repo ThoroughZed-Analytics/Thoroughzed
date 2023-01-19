abef9b650e1b0abca49d426d218a94a055f8cdcb
Jason Christopher <jchristopher2448@gmail.com>
2023-01-19 16:23:45 -0600

    more logic fixes for CLI

--- app/get_intrinsic_value.py
+++ app/get_intrinsic_value.py
@@ -7,16 +7,16 @@ def get_intrinsic_value(id, horse_cost):
     horse_object = get_summary_horse_data(id)
     horse = Horse(horse_object)
     total_races = horse.total_races
-    win_rate = horse.win_rate
-    place_rate = horse.place_rate
-    show_rate = horse.show_rate
+    win_rate = horse.win_rate / 100
+    second_place_rate = horse.second_place_finishes / total_races
+    third_place_rate = horse.third_place_finishes / total_races
     total_free_races = horse.total_free_races
     total_paid_races = horse.total_paid_races
     win_rate_free = horse.free_win_rate
     win_rate_paid = horse.paid_win_rate
 
     # Potential net earnings for each race (takes into account entry fee)
-    per_race_net_win = ((6 * win_rate / 100) + (3 * place_rate / 100) + (2 * show_rate / 100)) - 1
+    per_race_net_win = ((6 * win_rate) + (3 * second_place_rate) + (2 * third_place_rate)) - 1
 
     # 3-month yield (assuming 10 races per day)
     three_month_yield = (per_race_net_win * 90 * 10) / horse_cost * 100
