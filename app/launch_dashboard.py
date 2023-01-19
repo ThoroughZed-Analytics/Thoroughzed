326e296a10beef83296fd6dccc11fc0a2a2a330b
Jason Christopher <jchristopher2448@gmail.com>
2023-01-19 10:55:34 -0600

    added CLI fixes and created launch_dashboard file

---
+++ app/launch_dashboard.py
@@ -0,0 +1,127 @@
+from app.model_predict import predict_horse_price
+from app.meta_data_query_and_loop_script import get_summary_horse_data
+from app.Horse import Horse
+import pandas as pd
+import numpy as np
+import panel as pn
+import holoviews as hv
+import ssl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+from matplotlib.ticker import MaxNLocator
+pn.extension('tabulator')
+
+
+def launch_dashboard(id):
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        pass
+    else:
+        ssl._create_default_https_context = _create_unverified_https_context
+
+    # GET HORSE DATA FROM API
+
+    horse_object = get_summary_horse_data(id)
+    horse = Horse(horse_object)
+
+    ###############################################################################
+
+    # IMPORT AND CLEAN DATABASES
+
+    # clean_market_data = pd.read_csv('https://raw.githubusercontent.com/ThoroughZed-Analytics/Thoroughzed/dev/app/master_db_no_outliers.csv')
+    # clean_market_data = clean_market_data[~pd.isna(clean_market_data['converted_price'])]
+    market_data_no_outliers = pd.read_csv('https://raw.githubusercontent.com/ThoroughZed-Analytics/Thoroughzed/dev/app/master_db_no_outliers.csv')
+
+    by_breed = market_data_no_outliers.groupby('breed_type').mean().reset_index()
+    breed_daily = market_data_no_outliers.groupby(['day_sold', 'breed_type']).mean()
+    by_blood = market_data_no_outliers.groupby('bloodline').median().reset_index()
+    daily = market_data_no_outliers.groupby(['day_sold', 'bloodline']).mean()
+
+    ################################################################################
+
+    # BREED GRAPHS
+
+    barchart_win_rate_by_breed = sns.barplot(data=by_breed, x='breed_type', y='win_rate')
+    # barchart_win_rate_by_breed = barchart_win_rate_by_breed.plt.title('Mean Win Rate by Breed Type')
+
+    barchart_avg_win_num_by_breed = sns.barplot(data=by_breed, x='breed_type', y='overall.first')
+    # barchart_avg_win_num_by_breed = barchart_avg_win_num_by_breed.plt.title('Mean Number of 1st Place Finishes by Breed')
+
+    violin_usd_price_by_breed = sns.violinplot(data=market_data_no_outliers, x='breed_type', y='converted_price')
+
+    lineplot_sale_price_breed_time = sns.lineplot(data=breed_daily, x='day_sold', y='converted_price', hue='breed_type')
+    # lineplot_sale_price_breed_time = lineplot_sale_price_breed_time.plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True, prune='both'))
+    # lineplot_sale_price_breed_time = lineplot_sale_price_breed_time.plt.xticks(rotation=45)
+    # lineplot_sale_price_breed_time = lineplot_sale_price_breed_time.set(xlabel=None)
+    # lineplot_sale_price_breed_time = lineplot_sale_price_breed_time.plt.ylabel('Sale Price (USD)')
+    # lineplot_sale_price_breed_time = lineplot_sale_price_breed_time.plt.title('Horse Sale Price by Breed Over Time')
+
+    ##############################################################################
+
+    # BLOODLINE GRAPHS
+
+    barchart_median_win_by_blood = sns.barplot(data=by_blood, x='bloodline', y='win_rate')
+    # barchart_median_win_by_blood = barchart_median_win_by_blood.plt.title('Median Win Rate by Bloodline')
+    # barchart_median_win_by_blood = barchart_median_win_by_blood.plt.xticks(rotation=45)
+
+    lineplot_sale_price_blood_time = sns.lineplot(data=daily, x='day_sold', y='converted_price', hue='bloodline')
+    # lineplot_sale_price_blood_time = lineplot_sale_price_blood_time.plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True, prune='both'))
+    # lineplot_sale_price_blood_time = lineplot_sale_price_blood_time.plt.xticks(rotation=45)
+    # lineplot_sale_price_blood_time = lineplot_sale_price_blood_time.set(xlabel=None)
+    # lineplot_sale_price_blood_time = lineplot_sale_price_blood_time.plt.ylabel('Sale Price (USD)')
+    # lineplot_sale_price_blood_time = lineplot_sale_price_blood_time.plt.title('Horse Sale Price by Bloodline Over Time')
+
+    ###############################################################################
+
+    # DASHBOARD RENDERING
+
+    template = pn.template.FastListTemplate(
+        title='ThoroughZED Analytics - Relative Valuation', logo='https://files.slack.com/files-pri/T039KG69K-F04KLAMQJJU/image.png', header_background='black', header_color='red', font='times', shadow=True,
+        sidebar=[pn.pane.Markdown("## Horse Data"),
+                 pn.pane.Markdown(
+                     "#### Carbon dioxide emissions are the primary driver of global climate change. It’s widely recognised that to avoid the worst impacts of climate change, the world needs to urgently reduce emissions. But, how this responsibility is shared between regions, countries, and individuals has been an endless point of contention in international discussions."),
+                 pn.pane.PNG(horse.img_url, sizing_mode='scale_both'),
+                 pn.pane.Markdown("## Settings")],
+        main=[pn.Row(pn.Column(barchart_median_win_by_blood),
+                     barchart_avg_win_num_by_breed),
+              pn.Row(pn.Column(barchart_win_rate_by_breed),
+                     barchart_avg_win_num_by_breed)],
+        accent_base_color="#88d8b0",
+    )
+    template.show()
+
+    # ACCENT_COLOR = pn.template.FastGridTemplate.accent_base_color
+    # XS = np.linspace(0, np.pi)
+    #
+    # def sine(freq, phase):
+    #     return hv.Curve((XS, np.sin(XS * freq + phase))).opts(
+    #         responsive=True, min_height=400, title="Sine", color=ACCENT_COLOR
+    #     ).opts(line_width=6)
+    #
+    # def cosine(freq, phase):
+    #     return hv.Curve((XS, np.cos(XS * freq + phase))).opts(
+    #         responsive=True, min_height=400, title="Cosine", color=ACCENT_COLOR
+    #     ).opts(line_width=6)
+    #
+    # freq = pn.widgets.FloatSlider(name="Frequency", start=0, end=10, value=2)
+    # phase = pn.widgets.FloatSlider(name="Phase", start=0, end=np.pi)
+    #
+    # sine = pn.bind(sine, freq=freq, phase=phase)
+    # cosine = pn.bind(cosine, freq=freq, phase=phase)
+    #
+    # template = pn.template.FastGridTemplate(
+    #     site="ThoroughZED Analytics", title="Relative Valuation Dashboard",
+    #     header_color='red', header_background='black', theme='dark',
+    #     main_layout='card',
+    #     sidebar=[pn.pane.Markdown("## Settings"), freq, phase],
+    # )
+    #
+    # template.main[:3, :6] = pn.pane.HoloViews(hv.DynamicMap(sine), sizing_mode="stretch_both")
+    # template.main[:3, 6:] = pn.pane.HoloViews(hv.DynamicMap(cosine), sizing_mode="stretch_both")
+    # template.show()
+
+
+if __name__ == "__main__":
+    launch_dashboard(8919)
--- app/thoroughzed_cli.py
+++ app/thoroughzed_cli.py
@@ -4,6 +4,7 @@ from assets.art import art
 from app.get_relative_value import get_relative_value
 from app.model_predict import predict_horse_price
 from app.meta_data_query_and_loop_script import get_summary_horse_data
+from app.launch_dashboard import launch_dashboard
 import numpy as np
 import panel as pn
 import holoviews as hv
@@ -19,30 +20,35 @@ color_words = [colored('Horse/NFT ID', 'green', attrs=['bold']),
                colored("> *** Not a valid Horse/NFT ID in ZED Run ***", 'red', attrs=['bold']),
                colored("'y'", 'green', attrs=['bold']),
                colored("'n'", 'red', attrs=['bold']),
-               colored("> *** Type 'CTRL + C' to exit the online dashboard. ***", 'red', attrs=['blink'])]
+               colored("> *** Type 'CTRL + C' to exit the online dashboard. ***", 'red', attrs=['blink']),
+               colored("Intrinsic Valuation", 'green', attrs=['bold']),
+               colored("Relative Valuation", 'green', attrs=['bold']),
+               colored("NOTE", 'red', attrs=['blink']),
+               colored("Thank you for using ThoroughZED!", 'blue', attrs=['bold']),
+               colored("listing price", 'green', attrs=['bold'])]
 valid = True
 
 
 def help():
-    print("""
+    print(f"""
     How to Value a ZED Run Racehorse
     
     There are two ways to value a horse:
-    1. Intrinsic Valuation
+    1. {color_words[11]}
         - This model is based on how much money the horse is generating racing
         - The model will evaluate the provided horse id and listing price and provide the following:
             1. Expected Net Earnings / Race
             2. Expected 3-Month Yield
             3. Number of Races to Recoup the Cost of the Horse
         - For a more detailed explanation, please visit our Medium post here: https://tinyurl.com/how-to-value-ZED
-    2. Relative Valuation
+    2. {color_words[12]}
         - This model is based on comparable sales in the marketplace
         - The model will evaluate the provided horse id and take you to a dashboard to explore all sales of comparable horses
         - You will be able to see the the breakout of prices paid for similar horses
         - As a rule of thumb, you should not be will to pay more for a horse than the percentile of its win percentage
         - EX: If a horse has a 60th percentile win rate, you should not be willing to pay the 80th percentile price
     
-    NOTE: This model is a decision-making tool to assist in the evaluation of a given listing price or a bid you expect to put on a horse.
+    {color_words[13]}: This model is a decision-making tool to assist in the evaluation of a given listing price or a bid you expect to put on a horse.
         It is to be used to gauge the feasibility of making your money back on your investment given the price of the horse, however, this
         does not constitute financial advice nor guarantee the performance of the horse.
     """)
@@ -50,11 +56,11 @@ def help():
 
 def check_id(id):
     if id.lower() == "q":
-        print("> Thanks for using ThoroughZED!")
+        print(f"\n> {color_words[14]}\n")
         return "exit"
     while not id.isdigit():
         if id.lower() == "q":
-            print("> Thanks for using ThoroughZED!")
+            print(f"\n> {color_words[14]}\n")
             return "exit"
         if id.lower() == "h":
             help()
@@ -88,11 +94,11 @@ def check_valid_horse(id):
 
 def check_choice(choice):
     if choice.lower() == "q":
-        print("> Thanks for using ThoroughZED!")
+        print(f"\n> {color_words[14]}\n")
         return "exit"
     while choice.lower() != "r" and choice.lower() != "i":
         if choice.lower() == "q":
-            print("> Thanks for using ThoroughZED!")
+            print(f"\n> {color_words[14]}\n")
             return "exit"
         if choice.lower() == "h":
             help()
@@ -104,11 +110,11 @@ def check_choice(choice):
 
 def check_price(cost):
     if cost.lower() == "q":
-        print("> Thanks for using ThoroughZED!")
+        print(f"\n> {color_words[14]}\n")
         return "exit"
     while not cost.isdigit():
         if cost.lower() == "q":
-            print("> Thanks for using ThoroughZED!")
+            print(f"\n> {color_words[14]}\n")
             return "exit"
         else:
             print(f"\n{color_words[2]}\n")
@@ -118,46 +124,17 @@ def check_price(cost):
 
 def relative(id):
     result = predict_horse_price(int(id))
-    print(f"\n> Result: {result}\n")
+    if float(result) >= 0:
+        print('\n> Relative Value: ', colored(f'${result}', 'green'), '\n')
+    else:
+        print('\n> Relative Value: ', colored(f'${result}', 'red'), '\n')
     answer = input(f"> Would you like to display the dashboard? Type {color_words[8]} for yes or {color_words[9]} for no. ")
     while answer.lower() != 'y' and answer.lower() != 'n':
         print(f"\n{color_words[2]}\n")
         answer = input(f"> Would you like to display the dashboard? Type {color_words[8]} for yes or {color_words[9]} for no. ")
     if answer.lower() == 'y':
         print(f"\n{color_words[10]}\n")
-
-        def open_dash():
-            ACCENT_COLOR = pn.template.FastGridTemplate.accent_base_color
-            XS = np.linspace(0, np.pi)
-
-            def sine(freq, phase):
-                return hv.Curve((XS, np.sin(XS * freq + phase))).opts(
-                    responsive=True, min_height=400, title="Sine", color=ACCENT_COLOR
-                ).opts(line_width=6)
-
-            def cosine(freq, phase):
-                return hv.Curve((XS, np.cos(XS * freq + phase))).opts(
-                    responsive=True, min_height=400, title="Cosine", color=ACCENT_COLOR
-                ).opts(line_width=6)
-
-            freq = pn.widgets.FloatSlider(name="Frequency", start=0, end=10, value=2)
-            phase = pn.widgets.FloatSlider(name="Phase", start=0, end=np.pi)
-
-            sine = pn.bind(sine, freq=freq, phase=phase)
-            cosine = pn.bind(cosine, freq=freq, phase=phase)
-
-            template = pn.template.FastGridTemplate(
-                site="ThoroughZED Analytics", title="Relative Valuation Dashboard",
-                header_color='red', header_background='black', theme='dark',
-                main_layout='card',
-                sidebar=[pn.pane.Markdown("## Settings"), freq, phase],
-            )
-
-            template.main[:3, :6] = pn.pane.HoloViews(hv.DynamicMap(sine), sizing_mode="stretch_both")
-            template.main[:3, 6:] = pn.pane.HoloViews(hv.DynamicMap(cosine), sizing_mode="stretch_both")
-            template.show()
-
-        open_dash()
+        launch_dashboard(int(id))
 
 
 def intrinsic(id, cost):
@@ -191,12 +168,12 @@ def run_cli():
         else:
             choice = input(f"> Would you like to find the {color_words[3]} or {color_words[4]}? Or type {color_words[1]} to quit or {color_words[5]} for help. ")
             choice = check_choice(choice)
-            if check_choice(choice) == "exit":
+            if choice == "exit":
                 exit()
             if choice == "r":
                 relative(id)
             if choice == "i":
-                cost = input(f"> Enter the listing price of the horse in USD. Or type {color_words[1]} to quit. ")
+                cost = input(f"> Enter the {color_words[15]} of the horse in USD. Or type {color_words[1]} to quit. ")
                 cost = check_price(cost)
                 if cost == "exit":
                     exit()
@@ -206,7 +183,7 @@ def run_cli():
 if __name__ == "__main__":
     while True:
         run_cli()
-        run_again = input(f'> Enter another horse id? Type {color_words[8]} for yes or {color_words[9]} for no. ')
+        run_again = input(f'> Enter another {color_words[0]}? Type {color_words[8]} for yes or {color_words[9]} for no. ')
         if run_again.lower() == 'n':
-            print("> Thanks for using ThoroughZED!")
+            print(f"\n> {color_words[14]}\n")
             break
