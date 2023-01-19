201d0d1382a58504a249a1476a3f414b487139c0
Harper Foley <harper.e.foley@gmail.com>
2023-01-19 11:54:01 -0800

    Adds relative and intrinsic value to the sidebar.
    Also reduces font size of inline items

--- app/launch_dashboard.py
+++ app/launch_dashboard.py
@@ -1,5 +1,7 @@
 from app.model_predict import predict_horse_price
 from app.meta_data_query_and_loop_script import get_summary_horse_data
+from app.get_intrinsic_value import get_intrinsic_value
+from app.model_predict import predict_horse_price
 from app.Horse import Horse
 import pandas as pd
 import numpy as np
@@ -105,16 +107,22 @@ def launch_dashboard(id):
     # DASHBOARD RENDERING
 
     corrected_breed = horse.breed.capitalize()
+    intrinsic_value_lookup_result = get_intrinsic_value(int(horse.horse_id), 1) 
+    horse_expected_winnings_per_race = intrinsic_value_lookup_result[0]
+    relative_value = predict_horse_price(int(horse.horse_id))
 
     sidebar_horse_data_message = f"""
     # **{horse.name}**
     ## ***{horse.genotype}*** ***{horse.bloodline}*** ***{corrected_breed}***
-    * ## Win: *{"{0:.2f}".format(horse.win_rate)}%*
-    * ## Place: *{"{0:.2f}".format(horse.place_rate)}%*
-    * ## Show: *{"{0:.2f}".format(horse.show_rate)}%*
-    * ## Total Races: *{"{0:,}".format(horse.total_races)}*
-    * ## Free Win Rate: *{"{0:.2f}".format(horse.free_win_rate)}%*
-    * ## Paid Win Rate: *{"{0:.2f}".format(horse.paid_win_rate)}%*
+    * ### Win: *{"{0:.2f}".format(horse.win_rate)}%*
+    * ### Place: *{"{0:.2f}".format(horse.place_rate)}%*
+    * ### Show: *{"{0:.2f}".format(horse.show_rate)}%*
+    * ### Total Races: *{"{0:,}".format(horse.total_races)}*
+    * ### Free Win Rate: *{"{0:.2f}".format(horse.free_win_rate)}%*
+    * ### Paid Win Rate: *{"{0:.2f}".format(horse.paid_win_rate)}%*
+
+    ### Expected Net Winnings / Race: ${horse_expected_winnings_per_race}
+    ### Expected Market Value: ${relative_value}
     """
     template = pn.template.FastListTemplate(
         title='ThoroughZED Analytics - Relative Valuation', logo='https://i.imgur.com/3rpZHfT.png', header_background='black', header_color='red', font='times', shadow=True,
